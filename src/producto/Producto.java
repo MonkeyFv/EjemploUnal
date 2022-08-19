80204a0d315b444d481667265509c55a58b08cee
fabian1 <bfvargas83@gmail.com>
2022-08-18 20:15:29 -0500

    v3-Completamos Productos

---
+++ src/producto/Producto.java
@@ -0,0 +1,13 @@
+package producto;
+
+public class Producto {
+    private String nombre;
+    private  int precio;
+    private String categoria;
+
+    public Producto(String nombre, int precio, String categoria) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.categoria = categoria;
+    }
+}
